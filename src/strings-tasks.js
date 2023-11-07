7f5bb5b305fa33969713103f7affe3446632fe0b
4-13 <danilmatvienko@gmail.com>
2023-11-08 00:54:50 +0300

    feat: solve rot13

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -37,8 +37,12 @@ function getStringLength(value) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  if (typeof value === 'string' || value instanceof String) {
+    return true;
+  }
+
+  return false;
 }
 
 /**
@@ -470,8 +474,69 @@ function extractEmails(str) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13(/* str */) {
-  throw new Error('Not implemented');
+function encodeToRot13(str) {
+  const rot13eng = new Map([
+    ['A', 'N'],
+    ['B', 'O'],
+    ['C', 'P'],
+    ['D', 'Q'],
+    ['E', 'R'],
+    ['F', 'S'],
+    ['G', 'T'],
+    ['H', 'U'],
+    ['I', 'V'],
+    ['J', 'W'],
+    ['K', 'X'],
+    ['L', 'Y'],
+    ['M', 'Z'],
+    ['N', 'A'],
+    ['O', 'B'],
+    ['P', 'C'],
+    ['Q', 'D'],
+    ['R', 'E'],
+    ['S', 'F'],
+    ['T', 'G'],
+    ['U', 'H'],
+    ['V', 'I'],
+    ['W', 'J'],
+    ['X', 'K'],
+    ['Y', 'L'],
+    ['Z', 'M'],
+    ['a', 'n'],
+    ['b', 'o'],
+    ['c', 'p'],
+    ['d', 'q'],
+    ['e', 'r'],
+    ['f', 's'],
+    ['g', 't'],
+    ['h', 'u'],
+    ['i', 'v'],
+    ['j', 'w'],
+    ['k', 'x'],
+    ['l', 'y'],
+    ['m', 'z'],
+    ['n', 'a'],
+    ['o', 'b'],
+    ['p', 'c'],
+    ['q', 'd'],
+    ['r', 'e'],
+    ['s', 'f'],
+    ['t', 'g'],
+    ['u', 'h'],
+    ['v', 'i'],
+    ['w', 'j'],
+    ['x', 'k'],
+    ['y', 'l'],
+    ['z', 'm'],
+  ]);
+
+  const charToRot13 = (char, dictionary) => {
+    return dictionary.get(char) || char;
+  };
+
+  return str
+    .split('')
+    .reduce((res, char) => res + charToRot13(char, rot13eng), '');
 }
 
 /**
